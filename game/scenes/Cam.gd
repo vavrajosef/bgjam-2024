b48c4088385a8bd39a3b5f43bb0c312cd7e0f198
vavrajosef <vavra.pepa@gmail.com>
2024-08-09 21:47:16 +0200

    added camera, model and basic movement

---
+++ game/scenes/Cam.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_game_char_active():
+	var siblings := get_node("/root/Game").get_children()
+	var new_parent : Node
+	for sibling in siblings:
+		if sibling.name == "character-male-b2":
+			new_parent = sibling
+	get_parent().remove_child(self)
+	new_parent.add_child(self)
+
+func _on_game_char_inactive():
+	var new_parent := get_parent().get_parent()
+	get_parent().remove_child(self)
+	new_parent.add_child(self)
