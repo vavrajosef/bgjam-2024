bafbe49d3e1eb22d5b3cd4ca5f79196612fc59a0
vavrajosef <vavra.pepa@gmail.com>
2024-08-12 22:01:26 +0200

    added music to main menu

--- game/scenes/menu.gd
+++ game/scenes/menu.gd
@@ -1,9 +1,10 @@
 extends Control
 
+@onready var audio := %AudioPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	audio.start_playing()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
